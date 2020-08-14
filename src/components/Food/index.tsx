ba4fd81a935cc2878085b8c51f012999b2b86ddc
Jeniffer Azevedo <jeniqf@gmail.com>
2020-08-14 22:01:01 +0100

    Available food

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
+import api from '../../services/api';
 
 interface IFoodPlate {
   id: number;
@@ -27,7 +28,12 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    await api.put(`/foods/${food.id}`, {
+      ...food,
+      available: !isAvailable,
+      image: food.image,
+    });
+    setIsAvailable(!isAvailable);
   }
 
   function setEditingFood(): void {
