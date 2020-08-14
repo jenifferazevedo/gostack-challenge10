d653f3daebd38b47c2c54e8fe355f990468a1217
Jeniffer Azevedo <jeniqf@gmail.com>
2020-08-14 22:01:08 +0100

    delete food

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -79,7 +79,9 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+    const newFoodList = foods.filter(actualFood => actualFood.id !== id);
+    setFoods(newFoodList);
   }
 
   function toggleModal(): void {
